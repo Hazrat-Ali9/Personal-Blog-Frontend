65f1cd0ff49d705277f8b5f4a71e41af27d4f600
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-04-19 01:43:42 +0600

    Page tsx  🚂

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -3,7 +3,7 @@ export const metadata = {
   description:
     "Read our terms and conditions for using Hazrat Ali Blogs Website",
 };
-
+// Page tsx 
 export default function TermsPage() {
   return (
     <div className="container mx-auto max-w-4xl px-4 py-5">
