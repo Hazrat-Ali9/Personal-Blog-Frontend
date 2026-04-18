7fc884151e0fb3c372b5daa08dd8de80b5e7012c
GearHead87 <57955583+GearHead87@users.noreply.github.com>
2026-04-18 16:38:05 +0600

    Update next.config.ts to set output to "standalone" for improved build configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 // Ts config 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
